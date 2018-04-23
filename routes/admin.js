83305d5e30f5b8ccfa7217f06bc565905a5772d7
Mostafa Gad <Gad.mostafa@gmail.com>
2018-04-23 18:43:03 +0200

    Still working on the admin, not working yet

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,70 @@
 const express = require("express");
-
-const User= require("../models/user-model");
-
+const User= require("../models/User");
 const passport = require('passport');
+const multer = require("multer");
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+const Country = require('../models/Country');
+
+
+
+cloudinary.config ({
+  cloud_name: process.env.cloudinary_name ,
+  api_key: process.env.cloudinary_key,
+  api_secret: process.env.cloudinary_secret 
+});
+
+const storage = cloudinaryStorage({
+  cloudinary,
+  folder: "countries"
+});
+
+const upload = multer({ storage });
 
 const router = express.Router();
 
 
-router.get("/admin", (req,res,next)=>{
+router.get("/", (req,res,next)=>{
   if(!req.user) {
     next();
     return;
-  }
+  } 
+})
+
+  router.get("/admin/", (req,res,next) => {
+    res.render("admin/video");
+  });
+
+  router.get("/admin/", (req,res,next) => {
+    res.render("admin/video");
+  });
+  
+router.get("/add-country", (req,res,next) => {
+  res.render("country-form")
+})
+
+router.post("process-country", upload.single('blahUpload'), (req,res,next) => {
+  const {name, description, language, currency} = req.body;
+  const {originalname, secure_url} = req.file;
+
+  Country.create({
+    name, 
+    description,
+    language,
+    currency,
+    imageName: originalname,
+    imageUrl: secure_url
+  })
+   .then(()=> {
+     res.redirect("/");
+   })
+   .catch((err)=>{
+     next(err);
+   });
+   res.send(req.file);
+
+})
+
   User.find()
     .then((usersFromDb)=>{
       res.locals.userList = usersFromDb;
@@ -20,6 +73,5 @@ router.get("/admin", (req,res,next)=>{
     .catch((err)=>{
       next(err);
     });
-})
 
 module.exports = router;
