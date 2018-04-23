5fc9b13f0d60d658d41660506973554c7b785ed7
Patrycja <kucharskap@gmail.com>
2018-04-23 16:25:26 +0200

    login / see your account / edit your profile: visibility of those 3 in layout

--- passport/index.js
+++ passport/index.js
@@ -1,9 +1,14 @@
-const passport = require('passport');
+const passport = require("passport");
 
-require('./serializers');
-require('./localStrategy');
+require("./serializers");
+require("./localStrategy");
 
-module.exports = (app)  => {
+module.exports = app => {
   app.use(passport.initialize());
   app.use(passport.session());
-}
+
+  app.use((req, res, next) => {
+    res.locals.theUser = req.user;
+    next();
+  });
+};
