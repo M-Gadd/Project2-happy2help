4e008ac03550119cb839c79eb088665608cb7810
Patrycja <kucharskap@gmail.com>
2018-04-24 13:48:35 +0200

    edit user's password/ admin- removing users

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const authRoutes = express.Router();
 const User = require("../models/User");
 
@@ -7,17 +7,19 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
-
 authRoutes.get("/login", (req, res, next) => {
-  res.render("auth/login", { "message": req.flash("error") });
+  res.render("auth/login", { message: req.flash("error") });
 });
 
-authRoutes.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
+authRoutes.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
 
 authRoutes.get("/signup", (req, res, next) => {
   res.render("auth/signup");
@@ -55,13 +57,13 @@ authRoutes.post("/signup", (req, res, next) => {
       role,
       prefered_Language
     })
-    .then((userDet)=>{
-      console.log("created" + userDet.username);
-      res.redirect("/");
-    })
-    .catch(()=>{
-      console.log("error creating the user")
-    })
+      .then(userDet => {
+        console.log("created" + userDet.username);
+        res.redirect("/");
+      })
+      .catch(() => {
+        console.log("error creating the user");
+      });
   });
 });
 
@@ -70,35 +72,87 @@ authRoutes.get("/logout", (req, res) => {
   res.redirect("/");
 });
 
-authRoutes.get('/edit', (req, res, next)=>{
-  if(!req.user){
-    res.redirect('/auth/login');
+authRoutes.get("/edit", (req, res, next) => {
+  if (!req.user) {
+    res.redirect("/auth/login");
   }
-  res.render('auth/edit');
+  res.render("auth/edit");
 });
 
-authRoutes.post('/edit', (req, res, next)=>{
-  if(!req.user){
-    res.redirect('/auth/login');
+authRoutes.post("/edit", (req, res, next) => {
+  if (!req.user) {
+    res.redirect("/auth/login");
   }
- const { username, email, nationality, prefered_Language } = req.body;
+  const { username, email, nationality, prefered_Language } = req.body;
   User.findByIdAndUpdate(
     req.user._id,
     { username, email, nationality, prefered_Language },
-    { runValidators: true})
-  .then(()=>{
-    res.redirect("/auth/account-details");
-  })
-  .catch((err)=>{
-    next(err);
-  })
-})
+    { runValidators: true }
+  )
+    .then(() => {
+      res.redirect("/auth/account-details");
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
-authRoutes.get("/account-details", (req, res, next)=>{
-  if(!req.user){
-    res.redirect('/auth/login');
+authRoutes.get("/account-details", (req, res, next) => {
+  if (!req.user) {
+    res.redirect("/auth/login");
   }
-  res.render('auth/account-details');
+  res.render("auth/account-details");
+});
+
+authRoutes.get("/edit-password", (req, res, next) => {
+  if (!req.user) {
+    res.redirect("/auth/login");
+  }
+  res.render("auth/edit-password");
+});
+
+authRoutes.post("/edit-password", (req, res, next) => {
+  if (!req.user) {
+    res.redirect("/auth/login");
+  }
+
+  User.findById(req.user._id)
+    .then(userDetails => {
+      console.log(userDetails);
+      let { password } = userDetails;
+      const { passwordOld } = req.body;
+      if (!bcrypt.compareSync(passwordOld, password)) {
+        //FLASH! your old password is incorrect. try again
+        res.redirect("/auth/login");
+        return;
+      }
+      const { passwordNew, passwordNew1 } = req.body;
+      if (passwordNew !== passwordNew1) {
+        res.redirect("/auth/login");
+        //FLASH! you need to type your new password correctly twice
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(10);
+      password = bcrypt.hashSync(passwordNew1, salt);
+
+      User.findByIdAndUpdate(
+        req.user._id,
+        { password },
+        { runValidators: true }
+      )
+        .then(details => {
+          console.log(details);
+          res.redirect("/auth/account-details");
+          //FLASH! Congratulations! you changed your password
+        })
+        .catch(err => {
+          next(err);
+        });
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = authRoutes;
